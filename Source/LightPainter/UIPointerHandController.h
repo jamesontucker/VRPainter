e72ad37f8418b050f89e92a035a60adc83b65507
Jameson <jamesoncoletucker@yahoo.com>
2019-01-20 20:38:31 -0500

    Updated main menu grid, UI pointer hand controller, added a new game mode for main menu

--- Source/LightPainter/UIPointerHandController.h
+++ Source/LightPainter/UIPointerHandController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "HandControllerBase.h"
+#include "Components/WidgetInteractionComponent.h"
 #include "UIPointerHandController.generated.h"
 
 /**
@@ -15,6 +16,17 @@ class LIGHTPAINTER_API AUIPointerHandController : public AHandControllerBase
 	GENERATED_BODY()
 	
 	
-	
+public:
+	AUIPointerHandController();
+
+	void TriggerPressed() override;
+	void TriggerReleased() override;
+
+
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	UWidgetInteractionComponent* Pointer;
 	
 };
