72ed48d7c72bbb7973ec31dd05d8412c387196de
Jameson <jamesoncoletucker@yahoo.com>
2019-01-21 17:30:46 -0500

    Added save game index

--- Source/LightPainter/Saving/PainterSaveGame.cpp
+++ Source/LightPainter/Saving/PainterSaveGame.cpp
@@ -5,24 +5,34 @@
 #include "Kismet/GameplayStatics.h"
 #include "EngineUtils.h"
 #include "Misc/Guid.h"
-
+#include "PainterSaveGameIndex.h"
 #include "Stroke.h"
 
 UPainterSaveGame * UPainterSaveGame::Create()
 {
 	UPainterSaveGame* NewSaveGame = Cast<UPainterSaveGame>(UGameplayStatics::CreateSaveGameObject(StaticClass()));
 	NewSaveGame->SlotName = FGuid::NewGuid().ToString();
+	if (!NewSaveGame->Save()) return nullptr;
+
+	UPainterSaveGameIndex* Index = UPainterSaveGameIndex::Load();
+	Index->AddSaveGame(NewSaveGame);
+	Index->Save();
 	return NewSaveGame;
 }
 
 bool UPainterSaveGame::Save()
 {
+	UE_LOG(LogTemp, Warning, TEXT("Painting Index:"));
+	for (FString SlotName : UPainterSaveGameIndex::Load()->GetSlotNames())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Painting name: %s"), *SlotName);
+	}
 	return UGameplayStatics::SaveGameToSlot(this, SlotName, 0);
 }
 
 UPainterSaveGame * UPainterSaveGame::Load(FString SlotName)
 {
-	return Cast<UPainterSaveGame>(UGameplayStatics::LoadGameFromSlot(FString SlotName, 0));
+	return Cast<UPainterSaveGame>(UGameplayStatics::LoadGameFromSlot(SlotName, 0));
 }
 
 void UPainterSaveGame::SerializeFromWorld(UWorld * World)
