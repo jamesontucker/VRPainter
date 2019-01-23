3096ff1009f4f2ae8e3c9c85d6621f7b19d3fd77
Jameson <jamesoncoletucker@yahoo.com>
2019-01-22 20:08:16 -0500

    Added WBP menu to left hand controller

--- Source/LightPainter/UI/PaletteMenu/PaletteMenuHandController.cpp
+++ Source/LightPainter/UI/PaletteMenu/PaletteMenuHandController.cpp
@@ -2,6 +2,8 @@
 
 #include "PaletteMenuHandController.h"
 
-
-
-
+APaletteMenuHandController::APaletteMenuHandController()
+{
+	PaletteMenu = CreateDefaultSubobject<UWidgetComponent>(TEXT("PaletteMenu"));
+	PaletteMenu->SetupAttachment(GetRootComponent());
+}
